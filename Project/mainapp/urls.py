a65f6032bcf11a9bba1831f6bde319d345eac6b3
Iwona Strubczewska <iwona.strubczewska@gmail.com>
2018-04-18 22:05:46 +0200

    templates does not work properly

--- Project/mainapp/urls.py
+++ Project/mainapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^$', views.base, name='base'),
+    url(r'<int:pk>/base/', views.BaseView.as_view(), name='base'),
+    url(r'<int:pk>/home/', views.HomeView.as_view(), name='home'),
 ]
--- Project/mainapp/views.py
+++ Project/mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from numpy.core import generic
+from mainapp.models import Offer
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 
-# Create your views here.
-def home(request):
-    return render(request, 'mainapp/home.html', {})
+class HomeView(generic.DetailView):
+    model = Offer
+    template_name = 'mainapp/home.html'
 
-def base(request):
-    return render(request, 'mainapp/base.html', {})
 
+class BaseView(generic.DetailView):
+    model = Offer
+    template_name = 'mainapp/base.html'
